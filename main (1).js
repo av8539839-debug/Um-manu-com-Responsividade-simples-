323f6d2d335db930ba1d6d2d3afa91c461d6a26c
Ana Vitória <av8539839@gmail.com>
2025-11-15 13:35:18 -0300

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+const manuIcone = document.querySelector('.manu');
+manuIcone.addEventListener('click', () => {
+  
+menu.classList.toggle('active');
+    });
